16ab90be7384b2355c513321996d9d762ef074a4
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-05-05 19:38:59 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,14 @@
+let son1 = Number(prompt("Son1 ni kriting:"));
+let son2 = Number(prompt("Son2 ni kriting:"));
+
+function kattami (){
+    if(son1 > son2){
+            
+     alert(son1 + "katta" + son2 + "dan");
+    } else{
+
+     alert(son1 + "kichik" + son2 + "dan");
+    }
+}
+
+kattami();
